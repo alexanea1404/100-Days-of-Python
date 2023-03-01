c9b8e6486df609d3e9f7084411b2e952c7c64228
Alexandra Neagu <alexandra.neagu94@gmail.com>
2023-03-01 13:30:04 +0100

    Add Day1 of Python bootcamp

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to your band name generator")
+city = input("What is the city where you grew?\n")
+pet = input("What is your pet name?\n")
+print("Your band name is: " + city + " " + pet)
