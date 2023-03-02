a1566fc6d35c8ee9069ba4e4699171f92f64c829
Alexandra Neagu <alexandra.neagu94@gmail.com>
2023-03-02 11:33:59 +0100

    fixed bug

--- Day4/main.py
+++ Day4/main.py
@@ -29,18 +29,19 @@ scissors = '''
 
 
 choice_human=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-list_option=[rock,paper,scissors]
-print(list_option[choice_human])
-
-choice_computer=random.randint(0,len(list_option)-1)
-print("Computer chose")
-print(list_option[choice_computer])
-
 if choice_human >= 3 or choice_human <0 :
-  print("you typed and invalid number")
-elif choice_human == choice_computer:
-  print("Draw")
-elif (choice_human==0 and choice_computer==2) or (choice_human==1 and choice_computer==0) or (choice_human==2 and choice_computer==1):
-  print("You won")
+  print("You typed and invalid number")
 else:
-  print("You lose")
+  list_option=[rock,paper,scissors]
+  print(list_option[choice_human])
+
+  choice_computer=random.randint(0,len(list_option)-1)
+  print("Computer chose")
+  print(list_option[choice_computer])
+
+  if choice_human == choice_computer:
+    print("Draw")
+  elif (choice_human==0 and choice_computer==2) or (choice_human==1 and choice_computer==0) or (choice_human==2 and choice_computer==1):
+    print("You won")
+  else:
+    print("You lose")
